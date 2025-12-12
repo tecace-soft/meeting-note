9544eb541487dca8369dc8dcc04e46fa87727cc4
unknown <genekim@tecace.com>
2025-12-11 16:51:13 -0800

    Vite config setting update for Render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
   server: {
     port: 5174,
     open: true,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['meeting-note-fxms.onrender.com']
+  },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['meeting-note-fxms.onrender.com']
   }
 })
 
